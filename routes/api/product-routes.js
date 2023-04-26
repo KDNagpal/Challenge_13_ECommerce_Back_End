cd572993b00c96af42c085b727908666ac0623a5
KDNagpl <Karandeepnagpal2@gmail.com>
2023-04-26 16:55:50 -0400

    finshes up the tags route

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -4,15 +4,29 @@ const { Product, Category, Tag, ProductTag } = require('../../models');
 // The `/api/products` endpoint
 
 // get all products
-router.get('/', (req, res) => {
+router.get('/', async (req, res) => {
   // find all products
   // be sure to include its associated Category and Tag data
+  try{
+    const prodData = await Product.findAll({ include: {all: true, nested: true}});
+    res.status(200).json(prodData)
+  }
+  catch (err) {
+    res.status(500).json(err)
+  }
 });
 
 // get one product
-router.get('/:id', (req, res) => {
+router.get('/:id', async (req, res) => {
   // find a single product by its `id`
   // be sure to include its associated Category and Tag data
+  try{
+    const prodData = await Product.findByPk(req.params.id,{ include: {all: true, nested: true}});
+    res.status(200).json(prodData) 
+  }
+  catch (err) {
+    res.status(500).json(err)
+  }
 });
 
 // create new product
@@ -89,8 +103,18 @@ router.put('/:id', (req, res) => {
     });
 });
 
-router.delete('/:id', (req, res) => {
+router.delete('/:id', async (req, res) => {
   // delete one product by its `id` value
+  try{
+    const prodData = await Product.destroy({ where: {id: req.params.id}});
+    if (!prodData) {
+      res.status(404).json({message:'ID does not match any product.'})
+    }
+    res.status(200).json(`Product matching ID ${req.params.id} has been deleted.`)
+  }
+  catch (err) {
+    res.status(500).json(err)
+  }
 });
 
 module.exports = router;
